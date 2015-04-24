0301e01b346f15e4e6c6f8967141669d1ac9a6e0
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-04-24 17:45:27 +0100

    Introduce CashController
    to handle cash deposits

---
+++ BankingKata/Controllers/CashController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BankingKata.Controllers
+{
+    public class CashController : Controller
+    {
+        [HttpPost]
+        public ActionResult Deposit(FormCollection values)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
