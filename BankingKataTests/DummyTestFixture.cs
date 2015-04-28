8be73aa43b28e6ab3dea783258116fcc118f0eb5
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-04-28 14:10:10 +0100

    Add BankingKata and BankingKataTests projects
    with dummy class and dummy test as placeholders

---
+++ BankingKataTests/DummyTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    public class DummyTestFixture
+    {
+        [Test]
+        public void DummyTest()
+        {
+            var dummy = new DummyClass();
+            var result = dummy.IsDummy();
+            Assert.That(result, Is.True);
+        }
+    }
+}
