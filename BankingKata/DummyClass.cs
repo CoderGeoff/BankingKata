a91ae8fd6db435e87611b6676fe3e653785fb74c
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-04-28 16:34:32 +0100

    Clean up code (r#) and fix routes

--- BankingKata/DummyClass.cs
+++ BankingKata/DummyClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BankingKata
+﻿namespace BankingKata
 {
     public class DummyClass
     {
